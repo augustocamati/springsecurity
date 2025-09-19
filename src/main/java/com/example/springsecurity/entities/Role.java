aeab2489c525203be6d0ab4944e08add600874e4
Augusto camati <augustocamati21@gmail.com>
2025-09-19 21:42:58 +0100

    feate:atualizando a entidade Role para incluir mapeamento ManyToMany e renomeando tabela

--- src/main/java/com/example/springsecurity/entities/Role.java
+++ src/main/java/com/example/springsecurity/entities/Role.java
@@ -1,35 +1,40 @@
 package com.example.springsecurity.entities;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "tb_roles")
 public class Role {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "role_id")
   private Long id;
   private String name;
+  @ManyToMany(mappedBy = "roles")
+  private Set<User> users;
 
   public Long getId() {
-      return id;
+    return id;
   }
 
   public void setId(Long id) {
-      this.id = id;
+    this.id = id;
   }
 
   public String getName() {
-      return name;
+    return name;
   }
 
   public void setName(String name) {
-      this.name = name;
+    this.name = name;
   }
 
   public enum Values {
