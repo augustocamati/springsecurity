e5ed35cbb0d3bbef6d962c0b8a1fd969fb3f9d33
Augusto camati <augustocamati21@gmail.com>
2025-09-19 21:43:12 +0100

    feate:atualizando a entidade Tweet para corrigir mapeamento e renomear tabela

--- src/main/java/com/example/springsecurity/entities/Tweet.java
+++ src/main/java/com/example/springsecurity/entities/Tweet.java
@@ -1,68 +1,73 @@
 
 package com.example.springsecurity.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import java.time.Instant;
 
 @Entity
-@Table(name = "tweets")
+@Table(name = "tb_tweets")
 public class Tweet {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "tweet_id")
   private Long id;
   private String content;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
   private User author;
 
   private Instant createdAt;
 
-  
-
   public Tweet() {
-}
+  }
 
   public Tweet(Long id, String content, User author, Instant createdAt) {
     this.id = id;
     this.content = content;
     this.author = author;
     this.createdAt = createdAt;
-}
+  }
 
   // Getters and Setters
   public Long getId() {
-      return id;
+    return id;
   }
 
   public void setId(Long id) {
-      this.id = id;
+    this.id = id;
   }
+
   public Instant getCreatedAt() {
     return createdAt;
-}
+  }
 
   public void setCreatedAt(Instant createdAt) {
     this.createdAt = createdAt;
   }
 
-
   public String getContent() {
-      return content;
+    return content;
   }
 
   public void setContent(String content) {
-      this.content = content;
+    this.content = content;
   }
 
   public User getAuthor() {
-      return author;
+    return author;
   }
 
   public void setAuthor(User author) {
-      this.author = author;
+    this.author = author;
   }
 }
