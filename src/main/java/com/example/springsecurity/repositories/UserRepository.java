6475f886c5956584366b94e67e1be5895d3e9270
Augusto camati <augustocamati21@gmail.com>
2025-09-19 21:44:26 +0100

    feate:atualizando o repositório UserRepository para corrigir o tipo de ID de Long para UUID

--- src/main/java/com/example/springsecurity/repositories/UserRepository.java
+++ src/main/java/com/example/springsecurity/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,10 +10,10 @@ import com.example.springsecurity.entities.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
     
-    Optional<User> findByEmail(String email);
+    // Optional<User> findByEmail(String email);
     
   
 }
