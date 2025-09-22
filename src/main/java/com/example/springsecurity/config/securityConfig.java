87cbdb6b36056d545781549b990ca5aa0e51f060
Augusto camati <augustocamati21@gmail.com>
2025-09-23 00:30:47 +0100

    refact: permitir acesso público ao endpoint GET /tweets

--- src/main/java/com/example/springsecurity/config/securityConfig.java
+++ src/main/java/com/example/springsecurity/config/securityConfig.java
@@ -45,7 +45,8 @@ public class securityConfig {
         .authorizeHttpRequests(auth -> auth
 
             .requestMatchers(HttpMethod.POST, "/login").permitAll()
-            .requestMatchers(HttpMethod.POST, "/users").permitAll()
+            .requestMatchers(HttpMethod.POST, "/users").permitAll() 
+            .requestMatchers(HttpMethod.GET, "/tweets").permitAll()
 
             .anyRequest().authenticated())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
