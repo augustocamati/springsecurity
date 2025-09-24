c616f0f1d412469f5a4daae2902adf6f6be60f21
Augusto camati <augustocamati21@gmail.com>
2025-09-24 23:37:19 +0100

    feat: atualizar controlador para retornar DTOs na listagem de tweets

--- src/main/java/com/example/springsecurity/controller/ListTweetController.java
+++ src/main/java/com/example/springsecurity/controller/ListTweetController.java
@@ -1,14 +1,18 @@
 package com.example.springsecurity.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-// Update the import path to the correct location of ListTweetService
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.example.springsecurity.entities.Tweet;
+import com.example.springsecurity.dto.ResponseListTweetsDto;
 import com.example.springsecurity.services.ListTweetService;
 
 @Controller
@@ -22,8 +26,8 @@ public class ListTweetController {
 
   @GetMapping("/tweets")
   
-  public ResponseEntity<List<Tweet>> list() {
-    List<Tweet> tweets = listTweetService.execute();
+  public ResponseEntity<List<ResponseListTweetsDto>> list() {
+    List<ResponseListTweetsDto> tweets = listTweetService.execute();
  
     return ResponseEntity.ok(tweets);
   }
